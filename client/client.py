cbc446a88043c28018de5d7b2f9848e708d0d97c
asumin1982 <s_andriano@mail.ru>
2022-08-12 16:24:55 +0300

    Произведен рефакторинг, разобраться с многопоточностью и протестировать игру

--- client/client.py
+++ client/client.py
@@ -9,30 +9,32 @@ udp_max_size = 1024 # Максимальный размер UDP-пакета
 def listen(s: socket.socket):
     while True:
         msg = s.recv(udp_max_size)
-        print(f"\n{msg.decode('utf-8')}", sep='\n')
+        print(f"\n{msg.decode()}", sep='\n')
 #
 def connect(server: str, port: int):
     # создаем socket: (ipv4, udp)
     c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    name = input('You name: ')
+    name = input('Ваше имя: ')
 
     # connect
     c.connect((server, port))
-
+    #print(c)
     #
     threading.Thread(target=listen, args=(c,), daemon=True).start()
 
     # Пишем серверу, что соединились
-    c.send('__join'.encode('utf-8'))
+    c.send('__join'.encode())
+
 
     while True:
         msg = input(f"{name}: ")
-        c.send(msg.encode('utf-8'))
+        c.send(msg.encode())
 
 
 
 
 #
 if __name__ == '__main__':
+    os.system('clear')
     connect('127.0.0.1', 65000)
--- server/server.py
+++ server/server.py
@@ -18,27 +18,31 @@ def listen(host: str, port: int):
     flag = True
     while flag:
         msg, addr = s.recvfrom(udp_max_size)
-        # Добавляем клиентов в список
-        if addr not in clients:
-            clients.append(addr)
+        #print(msg, addr)
 
         # Уведомление о подключении клиентов
         client_ip = addr[0]
         client_port = addr[1]
-        if msg.decode('utf-8') == '__join':
-            print(f" [+] -> Клиент {client_ip}:{client_port} присоединился..")
+        if msg.decode() == '__join':
+            print(f" [+] -> Клиент {client_ip}:{client_port} присоединился")
+
+            # Добавляем клиентов в список
+            if addr not in clients:
+                clients.append(addr)
+            # print(clients)
             continue
 
         if not msg: # Если пустое сообщение
             continue
 
-        msg = f"Клиент {client_ip} прислал сообщение:\n-> {msg.decode('utf-8')}"
+        msg = f"Клиент {client_ip} прислал сообщение:\n-> {msg.decode()}"
 
         for client in clients:
+            #print(client)
             if client == addr: # Не отправлять данные клиенту, который их прислал
                 continue
             # Отправляем сообщение
-            s.sendto(msg.encode('utf-8'), client)
+            s.sendto(msg.encode(), client)
 
 #
 if __name__ == '__main__':
